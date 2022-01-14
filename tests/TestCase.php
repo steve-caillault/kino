6b3dec0b1336e438006cda3506876cb475cd3735
Stève Caillault <steve.caillault@gmail.com>
2022-01-14 15:04:46 +0100

    Nettoyage de la base de données après un grand nombre de tests.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,11 +6,18 @@ use Illuminate\Foundation\Testing\{
     TestCase as BaseTestCase,
     DatabaseTransactions
 };
+use Illuminate\Support\Facades\DB;
 use Faker\Generator as GeneratorFaker;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, DatabaseTransactions;
+
+    /**
+     * Nombre de tests exécutés
+     * @var int
+     */
+    private static int $count_tests_executed = 0;
 
     /**
      * Retourne une instance de Faker
@@ -22,4 +29,45 @@ abstract class TestCase extends BaseTestCase
         return with(new Faker)->get();
     }
 
+    /**
+     * Clean up the testing environment before the next test.
+     *
+     * @return void
+     *
+     * @throws \Mockery\Exception\InvalidCountException
+     */
+    protected function tearDown(): void
+    {
+        $this->beforeApplicationDestroyed(function() {
+            $this->cleanDatabases();
+
+            self::$count_tests_executed++;
+        });
+        parent::tearDown();
+    }
+
+    /**
+     * Tronque les tables de la base de données lors de l'exécution d'un grand nombre de tests
+     */
+    private function cleanDatabases() : void
+    {
+        $modelClassesToTruncate = [
+            \App\Models\AdminUser::class,
+            \App\Models\MovieRoom::class,
+        ];
+
+        // On ne tronque pas les tables à chaque fois
+        // Cela permet d'optimiser le temps d'exécution
+        if(self::$count_tests_executed % 10 === 0)
+        {
+            DB::statement('SET foreign_key_checks=0');
+
+            foreach($modelClassesToTruncate as $modelClassToTruncate)
+            {
+                with(new $modelClassToTruncate)->truncate();
+            }
+
+            DB::statement('SET foreign_key_checks=1');
+        }
+    }
 }
