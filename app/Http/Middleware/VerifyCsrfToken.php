81e7178ac7be7dbbcdba327a55efca70c139f453
Stève Caillault <steve.caillault@gmail.com>
2022-02-22 08:46:31 +0100

    Possibilité de vérifier le jeton CSRF lors des tests.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,9 +3,11 @@
 namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+use Illuminate\Http\Request;
 
-class VerifyCsrfToken extends Middleware
+final class VerifyCsrfToken extends Middleware
 {
+
     /**
      * The URIs that should be excluded from CSRF verification.
      *
@@ -14,4 +16,41 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    /**
+     * Requête courante
+     * @var Request
+     */
+    private Request $request;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     *
+     * @throws \Illuminate\Session\TokenMismatchException
+     */
+    public function handle(/*Request */$request, \Closure $next)
+    {
+        $this->request = $request;
+        return parent::handle($request, $next);
+    }
+
+    /**
+     * Determine if the application is running unit tests.
+     *
+     * @return bool
+     */
+    protected function runningUnitTests() : bool
+    {
+        if(! parent::runningUnitTests())
+        {
+            return false;
+        }
+
+        // Si on souhaite vérifier le jeton CSRF lors des tests
+        return ($this->request->session()->get('must_checked_csrf_token') !== true);
+    }
 }
