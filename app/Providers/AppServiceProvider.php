f41775c89dd7b3e9bf91853f65c417582b4eab59
Stève Caillault <steve.caillault@gmail.com>
2022-09-02 20:20:06 +0200

    Ajout d'une directive Blade pour retourner une URL de fichier d'assets avec Vite.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Pagination\Paginator;
+/***/
+use App\Vite;
 
-class AppServiceProvider extends ServiceProvider
+final class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
@@ -26,5 +29,11 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Paginator::useBootstrap();
+
+        // Directive Blade pour retourner l'URL d'un fichier d'assets
+        Blade::directive('vite_asset_url', function(string $path) {
+            return (new Vite())->getAssetUrl($path);
+        });
+
     }
 }
